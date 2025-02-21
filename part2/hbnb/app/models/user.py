7e88048d41148ba579339f2f726f26e4a79a4b8e
saru3450 <9971@holbertonstudents.com>
2025-02-21 15:19:59 +0000

    user.py

---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,7 @@
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
