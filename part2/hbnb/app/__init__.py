b1376bb790dc8185785bc5506cd14304633ee00c
saru3450 <9971@holbertonstudents.com>
2025-02-25 15:02:43 +0000

    __init__.py

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -5,7 +5,6 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
-    # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places, reviews, and amenities will be added later
-
+    # Register the users namespace
+    api.add_namespace(users_ns, path='/api/v1/users')
     return app
