271236a17801ae3c4502f80b45fd45350df21ad9
saru3450 <9971@holbertonstudents.com>
2025-02-27 15:38:55 +0000

    __init__.py

---
+++ part2/hbnb/app/api/__init__.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from flask_restx import Api
+from .v1.places import api as places_ns
+# from .v1.amenities import api as amenities_ns
+# from .v1.reviews import api as reviews_ns
+from .v1.users import api as users_ns
+
+# Initialize blueprint and API
+blueprint = Blueprint('api', __name__)
+api = Api(blueprint, title='HBnB API', version='1.0', description='API for HBnB')
+
+# Register namespaces
+api.add_namespace(places_ns, path='/api/v1/places')  # Maps /api/v1/places endpoints
+# api.add_namespace(amenities_ns, path='/api/v1/amenities')
+# api.add_namespace(reviews_ns, path='/api/v1/reviews')
+api.add_namespace(users_ns, path='/api/v1/users')
