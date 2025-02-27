8e53af3346f17b5240fcd5ad3030b76d659df661
saru3450 <9971@holbertonstudents.com>
2025-02-27 19:59:30 +0000

    user.py

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,3 +1,94 @@
+#!/usr/bin/python3
+"""This module for the Class User"""
+
+
+from .base import BaseModel
+import re
+
+
+class User(BaseModel):
+    """To create attibutes for the Class"""
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+        self.places = []
+
+    def add_place(self, place):
+        """This function to add places"""
+        self.places.append(place)
+        
+    def update(self, data):
+        if "first_name" in data:
+            self.first_name = data["first_name"]
+        if "last_name" in data:
+            self.last_name = data["last_name"]
+        if "email" in data:
+            self.email = data["email"]
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("First name must be a string")
+        if not value:
+            raise TypeError("First name is required")
+        if len(value) > 50:
+            raise ValueError("First name is too long")
+        self._first_name = value
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Last Name must be a string")
+        if not value:
+            raise TypeError("Last name is required")
+        if len(value) > 50:
+            raise ValueError("Last name is too long")
+        self._last_name = value
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Email must be a string")
+        if not value:
+            raise TypeError("Email is required")
+        if not re.match(
+            r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", value
+        ):
+            raise ValueError("Email is not valid")
+        self._email = value
+
+    @property
+    def is_admin(self):
+        return self._is_admin
+
+    @is_admin.setter
+    def is_admin(self, value):
+        if not isinstance(value, bool):
+            raise TypeError("Admin must be True or False")
+        self._is_admin = value
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+        }
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
