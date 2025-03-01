e1a85a5a797e740b1eaca8e9b768ee5924f06813
hugo-b-fx <gucci60610@gmail.com>
2025-03-01 15:00:47 +0100

    pycodestyle fixes

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -7,10 +7,10 @@ from .v1.users import api as users_ns
 
 # Initialize blueprint and API
 blueprint = Blueprint('api', __name__)
-api = Api(blueprint, title='HBnB API', version='1.0', description='API for HBnB')
+api = Api(blueprint, title='HBnB API', version='1.0', description='API')
 
 # Register namespaces
-api.add_namespace(places_ns, path='/api/v1/places')  # Maps /api/v1/places endpoints
+api.add_namespace(places_ns, path='/api/v1/places')
 # api.add_namespace(amenities_ns, path='/api/v1/amenities')
 # api.add_namespace(reviews_ns, path='/api/v1/reviews')
 api.add_namespace(users_ns, path='/api/v1/users')
