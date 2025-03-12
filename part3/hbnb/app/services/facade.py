8c01c1c2048a5c3e455aeed7509717fa047d12e8
saru3450 <9971@holbertonstudents.com>
2025-03-12 15:09:39 +0000

    facade.py

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,9 +1,100 @@
-# app/services/facade.py
+from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.amenity import Amenity
+from app.models.place import Place
+from app.models.review import Review
+from app.persistence.repository import SQLAlchemyRepository
+
+
 class HBnBFacade:
-    def create_user(self, data):
-        # Logic for creating user
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
+
+    # User method
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
         pass
 
     def get_user(self, user_id):
-        # Logic for getting user by ID
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def update_user(self, user_id, user_data):
+        self.user_repo.update(user_id, user_data)
+        return self.user_repo.get(user_id)
+
+    # Amenity method
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
         pass
+
+    def get_amenity_by_name(self, name):
+        return self.amenity_repo.get_by_attribute('name', name)
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+        pass
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+        pass
+
+    def update_amenity(self, amenity_id, amenity_data):
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.amenity_repo.get(amenity_id)
+        pass
+
+    # Place method
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+        pass
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+        pass
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+        pass
+
+    def update_place(self, place_id, place_data):
+        self.place_repo.update(place_id, place_data)
+        return self.place_repo.get(place_id)
+        pass
+
+    # review
+    def create_review(self, review_data):
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        return review
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
+
+    def get_reviews_by_place(self, place_id):
+        return self.review_repo.get_by_attribute('place_id', place_id)
+
+    def update_review(self, review_id, review_data):
+        self.place_repo.update(review_id, review_data)
+        return self.place_repo.get(review_id)
+
+    def delete_review(self, review_id):
+        self.review_repo.delete(review_id)
+        pass
+
+
