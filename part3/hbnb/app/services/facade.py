1bff521f02e0491c0d771d6800c4e57aa186dc97
saru3450 <9971@holbertonstudents.com>
2025-03-15 14:09:47 +0000

    facade.py

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -17,25 +17,22 @@ class HBnBFacade:
     # User method
     def create_user(self, user_data):
         user = User(**user_data)
-        self.user_repo.add(user)
+        self.user_repository.add(user)
         user.hash_password(user_data['password'])
-        self.user_repo.add(user)
+        self.user_repository.add(user)
         return user
 
     def get_user(self, user_id):
-        return self.user_repo.get(user_id)
+        return self.user_repository.get(user_id)
 
     def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute(email)
-
-    def get_user_by_email(self, email):
-        return self.user_repo.get_user_by_email(email)
+        return self.user_repository.get_by_attribute(email)
 
     def update_user(self, user_id, user_data):
         if "email" in user_data or "password" in user_data:
             raise ValueError("You cannot modify email or password.")
-        self.user_repo.update(user_id, user_data)
-        return self.user_repo.get(user_id)
+        self.user_repository.update(user_id, user_data)
+        return self.user_repository.get(user_id)
 
     def is_admin(self, user_id):
         user = self.get_user_by_id(user_id)
@@ -44,35 +41,35 @@ class HBnBFacade:
     # Amenity method
     def create_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
-        self.amenity_repo.add(amenity)
+        self.amenity_repository.add(amenity)
         return amenity
 
     def get_amenity_by_name(self, name):
-        return self.amenity_repo.get_by_attribute('name', name)
+        return self.amenity_repository.get_by_attribute('name', name)
 
     def get_amenity(self, amenity_id):
-        return self.amenity_repo.get(amenity_id)
+        return self.amenity_repository.get(amenity_id)
 
     def get_all_amenities(self):
-        return self.amenity_repo.get_all()
+        return self.amenity_repository.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-        self.amenity_repo.update(amenity_id, amenity_data)
-        return self.amenity_repo.get(amenity_id)
+        self.amenity_repository.update(amenity_id, amenity_data)
+        return self.amenity_repository.get(amenity_id)
 
     # Place method
     def create_place(self, place_data):
         if owner_id:
             place_data['owner_id'] = owner_id
         place = Place(**place_data)
-        self.place_repo.add(place)
+        self.place_repository.add(place)
         return place
 
     def get_place(self, place_id):
-        return self.place_repo.get(place_id)
+        return self.place_repository.get(place_id)
 
     def get_all_places(self):
-        return self.place_repo.get_all()
+        return self.place_repository.get_all()
 
     def update_place(self, place_id, place_data, user_id=None):
         place = self.get_place(place_id)
@@ -80,8 +77,8 @@ class HBnBFacade:
             raise ValueError("Place not found")
         if user_id and place.owner_id != user_id and not self.is_admin(user_id):
             raise ValueError("Unauthorized action")
-        self.place_repo.update(place_id, place_data)
-        return self.place_repo.get(place_id)
+        self.place_repository.update(place_id, place_data)
+        return self.place_repository.get(place_id)
 
     # review
     def create_review(self, review_data):
@@ -96,17 +93,17 @@ class HBnBFacade:
             raise ValueError("You have already reviewed this place.")
         review_data['user_id'] = user_id
         review = Review(**review_data)
-        self.review_repo.add(review)
+        self.review_repository.add(review)
         return review
 
     def get_review(self, review_id):
-        return self.review_repo.get(review_id)
+        return self.review_repository.get(review_id)
 
     def get_all_reviews(self):
-        return self.review_repo.get_all()
+        return self.review_repository.get_all()
 
     def get_reviews_by_place(self, place_id):
-        return self.review_repo.get_by_attribute('place_id', place_id)
+        return self.review_repository.get_by_attribute('place_id', place_id)
 
     def update_review(self, review_id, review_data):
         review = self.get_review(review_id)
@@ -114,8 +111,8 @@ class HBnBFacade:
             raise ValueError("Review not found")
         if user_id and review.user_id != user_id and not self.is_admin(user_id):
             raise ValueError("Unauthorized action")
-        self.place_repo.update(review_id, review_data)
-        return self.place_repo.get(review_id)
+        self.place_repository.update(review_id, review_data)
+        return self.place_repository.get(review_id)
 
     def delete_review(self, review_id):
         review = self.get_review(review_id)
@@ -123,5 +120,4 @@ class HBnBFacade:
             raise ValueError("Review not found")
         if user_id and review.user_id != user_id and not self.is_admin(user_id):
             raise ValueError("Unauthorized action")
-        self.review_repo.delete(review_id)
-
+        self.review_repository.delete(review_id)
