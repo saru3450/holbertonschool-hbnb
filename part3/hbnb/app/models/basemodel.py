fbc1bf4dfa183da7513bb9042af46486c072b597
saru3450 <9971@holbertonstudents.com>
2025-04-03 14:00:54 +0000

    basemodel.py

--- part3/hbnb/app/models/basemodel.py
+++ part3/hbnb/app/models/basemodel.py
@@ -18,15 +18,18 @@ class BaseModel(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def __init__(self):
-        # Initialise un nouvel objet avec un ID unique et des timestamps
-        self.id = str(uuid.uuid4())  # Génère un identifiant unique
-        self.created_at = datetime.now()  # Stocke la date et l'heure de création
-        self.updated_at = datetime.now()  # Stocke la date et l'heure de la dernière mise à jour
+    def __init__(self, **kwargs):  # Permet d'accepter les arguments passés
+        super().__init__(**kwargs)  # Appelle SQLAlchemy pour gérer les attributs
+        if not self.id:
+            self.id = str(uuid.uuid4())  # Génère un identifiant unique si absent
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
     
     def save(self):
         '''Update the `updated_at` timestamp to the current time.'''
         self.updated_at = datetime.now()
+        db.session.add(self)
+        db.session.commit()
 
     def update(self, data):
         '''Update instance attributes based on the given dictionary : data'''
